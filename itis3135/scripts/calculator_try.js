4b9d2189375a8050b52bc004cf79eb77043c91dd
Dixon Ohair <dohair@uncc.edu>
2021-09-28 18:29:28 -0400

    Update calculator_try.js

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,22 +1,7 @@
-var name = prompt("Hello! What is your name?");
-	var mood = prompt("How are you today?");
-	var time = new Date();
-	document.write("The Disfunctional Ostrich welcomes you, " + name + "!" + " We're glad you are doing " + mood +"!");
-	document.write("\n\n\n");
-	document.write("Today is " + time.toDateString() + ". The Time is " + time.getHours() +
-		 ":" + time.getMinutes() + ".");
 
 
 function option1(){
-	var option = prompt("Would you like to own an ostrich?");
-	if(option.toLowerCase().includes("yes")){
-		var why = prompt("Why would you like to own an ostrich?");
-		document.getElementById("option1").innerHTML = ("OMG! That is a great reason!");
-	}else if(option.toLowerCase().includes("no")){
-		document.getElementById("option1").innerHTML = ("OMG! You totally should own an ostrich!");
-	}else{
-		document.getElementById("option1").innerHTML = ("You need more ostriches.");
-	}
+	
 }
 
 function option2(){
