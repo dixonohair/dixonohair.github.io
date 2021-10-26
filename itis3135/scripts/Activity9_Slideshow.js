459642636f654dec55916e7b2cacb4c91bfe9285
Dixon Ohair <dohair@uncc.edu>
2021-10-26 17:52:51 -0400

    Update Activity9_Slideshow.js

--- itis3135/scripts/Activity9_Slideshow.js
+++ itis3135/scripts/Activity9_Slideshow.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         var imageLocation = $(this).attr("href");
         $("#firstImage").attr("src", imageLocation);
 
-        evt.preventDefault();
+       evt.preventDefault();
     });
     
     $("li:first-child a").focus();
