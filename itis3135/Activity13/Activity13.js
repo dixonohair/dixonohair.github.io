a3639d9b60ea665e1ae23532d02fc2a7e95c9053
Dixon Ohair <dohair@uncc.edu>
2021-12-12 23:32:49 -0500

    Create Activity13.js

---
+++ itis3135/Activity13/Activity13.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "team.json",
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#team").html("");
+            $.each(data, function(){
+                $.each(this, function(key, value){
+                    $("#team").append("<h3>" + value.name + "</h3><br>" + "<p>Title: " + value.title + "<br>" + "Bio: " + value.bio + "</p><br>");
+                });
+            });
+        }
+    });
+});
